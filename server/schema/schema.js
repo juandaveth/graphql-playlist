6b5591cf8ef5b8bc4d6867d10811b80dc965edda
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-23 15:36:13 -0500

    13.1 - Adding dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,11 +11,11 @@ const {
 
 // dummy data to test episodes
 var episodes = [
-  {name: 'Joe Rogan Experience #1309 - Naval Ravikant', podcast: 'The Joe Rogan Experience', id: '1'},
-  {name: '#343: Seth Godin on How to Say “No,” Market Like a Professional, and Win at Life', podcast: 'The Tim Ferriss Show', id: '2'},
-  {name: '#376: How Seth Godin Manages His Life — Rules, Principles, and Obsessions (Repost)', podcast: 'The Tim Ferriss Show', id: '3'},
-  {name: '#11 Media Manipulator with Ryan Holiday', podcast: 'The Knowledge Project with Shane Parris', id: '4'},
-  {name: '#18 Naval Ravikant — The Angel Philosopher', podcast: 'The Knowledge Project with Shane Parris', id: '5'},
+  {name: 'Joe Rogan Experience #1309 - Naval Ravikant', podcast: 'The Joe Rogan Experience', id: '1', mentorId: '1'},
+  {name: '#343: Seth Godin on How to Say “No,” Market Like a Professional, and Win at Life', podcast: 'The Tim Ferriss Show', id: '2', mentorId: '2'},
+  {name: '#376: How Seth Godin Manages His Life — Rules, Principles, and Obsessions (Repost)', podcast: 'The Tim Ferriss Show', id: '3', mentorId: '2'},
+  {name: '#11 Media Manipulator with Ryan Holiday', podcast: 'The Knowledge Project with Shane Parris', id: '4', mentorId: '3'},
+  {name: '#18 Naval Ravikant — The Angel Philosopher', podcast: 'The Knowledge Project with Shane Parris', id: '5', mentorId: '1'},
 ]
 
 // dummy data to mentors
@@ -31,6 +31,7 @@ const EpisodeType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     podcast: { type: GraphQLString },
+   
   })
 });
 
@@ -50,7 +51,6 @@ const RootQuery = new GraphQLObjectType({
       type: EpisodeType,
       args: { id: {type: GraphQLID} },
       resolve(parent, args){
-        // here we will retrieve info from some db
         console.log(typeof(args.id));
         return _.find(episodes, {id: args.id});
       }
