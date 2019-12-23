4ad7cd50bb35f6526d051f4e3bcc8645dffc027a
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-23 11:18:20 -0500

    9.1 - Adding dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,8 +1,15 @@
 const graphql = require('graphql');
 
-// Firt we'll define one object type
-// Using ES6 destructuring
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data to test episodes
+var episodes = [
+  {name: 'Joe Rogan Experience #1309 - Naval Ravikant', podcast: 'The Joe Rogan Experience', id: '1'},
+  {name: '#343: Seth Godin on How to Say “No,” Market Like a Professional, and Win at Life', podcast: 'The Tim Ferriss Show', id: '2'},
+  {name: '#376: How Seth Godin Manages His Life — Rules, Principles, and Obsessions (Repost)', podcast: 'The Tim Ferriss Show', id: '3'},
+  {name: '#11 Media Manipulator with Ryan Holiday', podcast: 'The Knowledge Project with Shane Parris', id: '4'},
+  {name: '#18 Naval Ravikant — The Angel Philosopher', podcast: 'The Knowledge Project with Shane Parris', id: '5'},
+]
 
 const EpisodeType = new GraphQLObjectType({
   name: 'Episode',
