0be996911318746ea965985ce1bdc03137ee3bd1
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-22 21:08:49 -0500

    7.2 - Destructuring GraphQLString to define Object Type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,3 +1,14 @@
 const graphql = require('graphql');
 
 // Firt we'll define one object type
+// Using ES6 destructuring
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields:() => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  })
+});
