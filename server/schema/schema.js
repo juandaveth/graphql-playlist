43ecd9b205d9b120272b48d1d1f1dd96d466fb02
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-24 10:13:59 -0500

    17.2.1 - Deleting dummy data

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,8 +10,8 @@ const {
   GraphQLList,
 } = graphql;
 
-// dummy data to test episodes
-var episodes = [
+// dummy data to test episodes --> Going to MongoDB
+/* var episodes = [
   {name: 'Joe Rogan Experience #1309 - Naval Ravikant', podcast: 'The Joe Rogan Experience', id: '1', mentorId: '1'},
   {name: '#343: Seth Godin on How to Say “No,” Market Like a Professional, and Win at Life', podcast: 'The Tim Ferriss Show', id: '2', mentorId: '2'},
   {name: '#376: How Seth Godin Manages His Life — Rules, Principles, and Obsessions (Repost)', podcast: 'The Tim Ferriss Show', id: '3', mentorId: '2'},
@@ -20,14 +20,14 @@ var episodes = [
   {name: 'Seth Godin: Purple Cows', podcast: 'North Star Podcast', id: '6', mentorId: '2'},
   {name: 'Ryan Holiday: Timeless Lessons From History', podcast: 'North Star Podcast', id: '7', mentorId: '3'},
   {name: 'Ryan Holiday: Using Practical Ancient Philosophy as a Guide to Achieving Mastery', podcast: 'The School of Greatness', id: '8', mentorId: '3'},
-]
+] */
 
-// dummy data to mentors
-var mentors = [
+// dummy data to mentors --> Going to MongoDB
+/* var mentors = [
   {name: 'Naval Ravikant', age: 45, id: '1'},
   {name: 'Seth Godin', age: 59, id: '2'},
   {name: 'Ryan Holiday', age: 32, id: '3'},
-]
+]*/
 
 const EpisodeType = new GraphQLObjectType({
   name: 'Episode',
