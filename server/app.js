c859b9683c24d9cc8573f49f2620667acfbe9844
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-24 09:16:43 -0500

    16.1 - Adding mongoose, connecting to DB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema')
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//For testing purposes, I need to figure out a way to hide it
+mongoose.connect('mongodb+srv://jureyes:ez1BO1dQb0dfFw7S@gql-ninja-z1y2x.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+  console.log('connected to DB!!')
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
