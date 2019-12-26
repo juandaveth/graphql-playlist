99d2614d5b641bef059b65326a0c6093036a584e
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-26 17:06:07 -0500

    25.2 - Importing ApolloClient with the corresponding single endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+
 import EpisodeList from './components/EpisodeList';
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql';
+})
 
 function App() {
   return (
