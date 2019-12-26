5522dc0ee8cca256b56d3fc23f65eeb76de979a1
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-26 16:46:13 -0500

    24.1 - Create a basic component for EpisodeList

---
+++ client/src/components/EpisodeList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function EpisodeList() {
+  return (
+      <div>
+        <ul id="episode-list">
+            <li>Hello World!</li>
+        </ul>
+      </div>
+  );
+}
+
+export default EpisodeList;
