32c2ca3d1adc93074b8b20557f9012b4c4134000
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-27 16:52:12 -0500

    26.4 - Using cors for cross-origin requests

--- server/app.js
+++ server/app.js
@@ -2,13 +2,17 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+
+// Allowing cross-origin requests
+app.use(cors());
 
 //For testing purposes, I need to figure out a way to hide it
 mongoose.connect('mongodb+srv://jureyes:ez1BO1dQb0dfFw7S@gql-ninja-z1y2x.mongodb.net/test?retryWrites=true&w=majority');
 mongoose.connection.once('open', () => {
-  console.log('connected to DB!!')
+  console.log('connected to mongoDB!!')
 })
 
 app.use('/graphql', graphqlHTTP({
