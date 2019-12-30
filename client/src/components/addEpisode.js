402df424a1edcd594bd7fb0a8e92757fcd14f5d2
jdreyespaez <jd.reyespaez@gmail.com>
2019-12-30 14:55:16 -0500

    28.2 - Add the basic structure for the form

--- client/src/components/addEpisode.js
+++ client/src/components/addEpisode.js
@@ -11,3 +11,31 @@ const getMentorsQuery = gql`
     }
 `
 
+class AddMentor extends Component {
+    render(){
+        return (
+            <form id="add-episode">
+
+                <div className="field">
+                    <label>Episode name:</label>
+                    <input type="text"/>
+                </div>
+
+                <div className="field">
+                    <label>Podcast:</label>
+                    <input type="text"/>
+                </div>
+
+                <div className="field">
+                    <label>Mentor:</label>
+                    <select>
+                        <option>Select mentor</option>
+                    </select>
+                </div>
+
+                <button> + </button>
+
+            </form>
+      );
+    }
+}
